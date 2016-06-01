6c42fec129db5c6e8b6831b7b822935c6d521b1f
Branden Romero <branden.r.romero@gmail.com>
2016-06-01 13:36:02 -0600

    added distribution based coherence metrics

--- evaluation_metrics/coherence/coherence.py
+++ evaluation_metrics/coherence/coherence.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.misc
+import math
 
 class Coherence():
 	def __init__(self,type='default',M=20):
@@ -22,36 +24,82 @@ def oc_auto_pmi(Pwjwi,Pwi,Pwj):
 def oc_auto_npmi(Pwjwi,Pwi,Pwj):
 	return (np.log(Pwjwi/(Pwi*Pwj)))/(-np.log(Pwjwi))
 
-def oc_auto_lcp(Pwjwi,Pwi,Pwj):
+def oc_auto_lcp(Pwjwi,Pwi):
 	return np.log(Pwjwi/Pwi)
 
-def coherence(coefs,X,M,type):
+def sim_cos(wi,wj):
+	return np.dot(wi,wj)/(np.linalg.norm(wi)*np.linalg.norm(wj))
+
+def sim_dice(wi,wj):
+	N = wi.shape[0]
+	return (2*np.sum([min(wi[n],wj[n]) for n in xrange(N)]))/np.sum(wi+wj)
+		
+def sim_jaccard(wi,wj):
+	N = wi.shape[0]
+	return np.sum([min(wi[n],wj[n]) for n in xrange(N)])/np.sum([max(wi[n],wj[n]) for n in xrange(N)])
+
+def oc_auto_ds(wi,wj,dist=sim_cos):
+	return dist(wi,wj)
+	
+	
+
+def coherence(coefs,X,M,type,dist='cosine'):
 	numDocs = X.shape[0]
 	coherenceType = {
 			'default': topic_coherence,
 			'OC-Auto-PMI': oc_auto_pmi,
 			'OC-Auto-NPMI': oc_auto_npmi,
-			'OC-Auto-LCP': oc_auto_lcp
+			'OC-Auto-LCP': oc_auto_lcp,
+			'OC-Auto-DS' : oc_auto_ds
 	}
+
+	if type == 'OC-Auto-DS':
+		distributionType = {
+					'cosine': sim_cos:
+					'dice': sim_dice:
+					'jaccard': sim_jaccard:
+		}	
 
 	coherenceMetric = coherenceType[type]
 	topTokens = (-coefs).argsort()[:M][0]
-	cohere = 0
+	cohere = 0	
 
 	for m in xrange(1,M):
 		for l in xrange(0,m-1):
-			vm = np.nonzero(X[:,topTokens[m]])[0].shape[0]
-			vl = np.nonzero(X[:,topTokens[l]])[0].shape[0]
-			vmvl = np.intersect1d(vm,vl).shape[0]
 			if type == 'default':
+				vm = np.nonzero(X[:,topTokens[m]])[0].shape[0]
+				vl = np.nonzero(X[:,topTokens[l]])[0].shape[0]
+				vmvl = np.intersect1d(vm,vl).shape[0]
 				cohere += coherenceMetric(vmvl,vm,vl)
+
+			elif type == 'OC-Auto-DS':
+				wi = (topTokens[m] > 0).astype(np.int)
+				wj = (topTokens[l] > 0).astype(np.int)
+				distribution = distributionType[dist]
+				cohere += coherenceMetric(wi,wj,distribution)
+
+			elif type == 'OC-Auto-LCP':
+				vm = np.nonzero(X[:,topTokens[m]])[0].shape[0]
+				vl = np.nonzero(X[:,topTokens[l]])[0].shape[0]
+				vmvl = np.intersect1d(vm,vl).shape[0]
+				#Added smoothing factor
+				Pwjwi = (vmvl+1)/float(numDocs)
+				Pwi = (vl+1)/float(numDocs)
+				cohere += coherenceMetric(Pwjwi,Pwi)
+				
 			else:
+				vm = np.nonzero(X[:,topTokens[m]])[0].shape[0]
+				vl = np.nonzero(X[:,topTokens[l]])[0].shape[0]
+				vmvl = np.intersect1d(vm,vl).shape[0]
 				#Added smoothing factor
 				Pwjwi = (vmvl+1)/float(numDocs)
 				Pwi = (vl+1)/float(numDocs)
 				Pwj = (vm+1)/float(numDocs)
 				cohere += coherenceMetric(Pwjwi,Pwi,Pwj)
 
+	if type == 'OC-Auto-DS':
+		cohere = cohere/scipy.misc(M,2)
+		
 	return cohere	
 
 
